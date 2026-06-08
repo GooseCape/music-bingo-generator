b1c6c1e1da6050018122adf865e00017fc4137e4
GooseCape <130437141+GooseCape@users.noreply.github.com>
2026-06-08 12:33:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,9 +103,9 @@ def generate_bingo_pdf(songs_list, num_cards, bingo_title):
                 card_data = generate_bingo_card(songs_list)
 
                 c.setFont("Helvetica-Bold", 14)
-                c.drawCentredString(x + card_width/2, y + card_height - 8*mm, bingo_title)
+                c.drawCentredString(x + card_width/2, y + card_height - 2*mm, bingo_title)
                 c.setFont("Helvetica", 11)
-                c.drawCentredString(x + card_width/2, y + card_height - 18*mm, f"Card #{cards_generated + 1}")
+                c.drawCentredString(x + card_width/2, y + card_height - 120*mm, f"Card #{cards_generated + 1}")
 
                 t = Table(card_data, 
                          colWidths=[card_width/5.05]*5, 
